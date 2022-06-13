7ed3d354ad64bdd1e0efde28c4e529dece5030e9
Francisco V <Irontacolol@gmail.com>
2022-06-13 19:49:33 -0400

    Placeholder TickManager class.

---
+++ Assets/Manager/TickManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Manager
+{
+    //Handles time that passes in-game.
+    public class TickManager : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
