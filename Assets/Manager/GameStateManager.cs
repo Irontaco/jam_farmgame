8c77797c2132a9002bf8a86a044852d260838c54
Francisco V <Irontacolol@gmail.com>
2022-06-13 21:43:09 -0400

    debug text for soil objects

--- Assets/Manager/GameStateManager.cs
+++ Assets/Manager/GameStateManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 //GameManager should be used exclusively to instantiate everything we need in an orderly fashion.
@@ -9,11 +10,20 @@ public class GameStateManager : MonoBehaviour
 
     public WorldTileManager WorldTileManager { get; private set; }
 
+    public TickManager TickManager { get; private set; }
+
     public InputManager InputManager { get; private set; }
     
     public WorldData CurrentWorldData { get; private set; }
 
     public Config CurrentConfig { get; private set; } = Config.Default;
+
+    //temp
+    public Tile LastSelectedTile;
+    public GameObject TileSelectText;
+
+    public Canvas SoilDebugCanvas;
+    public List<GameObject> SoilDebugTextGroup;
 
     void Start()
     { 
@@ -24,20 +34,65 @@ public class GameStateManager : MonoBehaviour
         WorldTileManager = new WorldTileManager(CurrentWorldData);
 
         InputManager = new InputManager(this);
+
+        TickManager = new TickManager();
+
+        SoilDebugTextGroup = new List<GameObject>();
+
+
     }
 
     private void FixedUpdate()
     {
-        var tile = InputManager.GetTileAtMousePosition(out var rayHitPos);
+        Vector3 currMousePos = Input.mousePosition;
+
+        var tile = LastSelectedTile = InputManager.GetTileAtMousePosition(out var rayHitPos);
+
+        foreach (GameObject gameObject in SoilDebugTextGroup)
+        {
+            Destroy(gameObject);
+        }
+        SoilDebugTextGroup.Clear();
+
         if (tile != null)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             Debug.DrawLine(ray.origin, rayHitPos);
 
-            Debug.Log($"TILEPOS X = [{tile.X}] Z = [{tile.Z}]");
-            Debug.Log($"RAYHITPOS X = [{Mathf.RoundToInt(rayHitPos.x)}] Z = [{Mathf.RoundToInt(rayHitPos.z)}]");
             WorldTileManager.ChangeTileType(tile, TileType.Virtual);
             WorldTileManager.WorldMeshData.UpdateWorldMesh();
+
+            //Display tile value on TileSelectText
+            TileSelectText.GetComponent<TextMeshProUGUI>().text = tile.Soil.Examine;
+            TileSelectText.transform.position = new Vector3(currMousePos.x + 80, currMousePos.y + 60, currMousePos.z);
+
+            //Draw Text on a 3x3 tile radius showing soil main value
+            List<GameObject> NewSoilDebugTextGroup = new List<GameObject>();
+
+            foreach(Soil soil in tile.Soil.SoilNeighbors)
+            {
+                if(soil != null)
+                {
+                    GameObject SoilText = new GameObject();
+                    SoilText.AddComponent<TextMeshPro>().transform.SetParent(SoilDebugCanvas.transform);
+                    SoilText.transform.name = "DebugSoilText";
+                    SoilText.GetComponent<RectTransform>().sizeDelta = new Vector2(1, 1);
+                    SoilText.transform.position = new Vector3(soil.X, soil.Y + 0.1f, soil.Z);
+                    SoilText.transform.Rotate(new Vector3(90, 0, 0));
+
+                    SoilText.GetComponent<TextMeshPro>().fontSize = 8;
+                    SoilText.GetComponent<TextMeshPro>().alignment = TextAlignmentOptions.Center;
+                    SoilText.GetComponent<TextMeshPro>().text = soil.AvailableEnergy.ToString();
+
+                    NewSoilDebugTextGroup.Add(SoilText);
+                }
+            }
+
+            SoilDebugTextGroup = NewSoilDebugTextGroup;
+            
+            WorldTileManager.ChangeTileType(LastSelectedTile, TileType.Floor);
+
         }
+
     }
 }
