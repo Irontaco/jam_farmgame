d443dd946a04ba268e7fb0ae6c986809c57a9f62
Francisco V <Irontacolol@gmail.com>
2022-06-17 01:14:01 -0400

    Updated common value interfaces

---
+++ Assets/Data/MapObjects/Interfaces/IHealth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Base vars and methods for health management in entities.
+internal interface IHealth
+{
+
+    float Health { get; set; }
+    float CurrentHealth { get; set; }
+    bool Dead { get; set; }
+    bool Shielded { get; set; }
+
+    void TakeDamage(float dmgAmmount);
+    void Heal(float healAmmount);
+    void Die();
+
+}
---
+++ Assets/Data/MapObjects/Interfaces/IMovement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+//Handles movement modifiers for anything that is supposed to move.
+interface IMovement
+{
+
+    float BaseMovementSpeed { get; set; }
+
+    //result of any modifications on the BaseMovementSpeed
+    float CurrentMovementSpeed { get; set; }
+
+    //final multiplier applied onto BaseMovementSpeed
+    float CummulativeMovementSpeedMultiplier { get; set; }
+
+    float ApplyMovementSpeedModifier(float modifier);
+    void FreezeInPlace(float freezeTime);
+
+    //Make this entity move towards a specific column of tiles.
+    void SwitchLanes(Vector3 moveTowards);
+
+    //Use this for any detours or special movements that the entity should make.
+    void PerformMovementSpecial();
+
+    //when we are instantiated or we are stopped from walking by something - continue moving downwards.
+    void BeginWalkingFowards();
+
+}
